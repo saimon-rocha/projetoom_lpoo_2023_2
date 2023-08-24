99bf17341b25e8358e33427bb0d0b94b9d38800c
saimon rocha <saimon.rocha07@gmail.com>
2023-08-24 10:52:29 -0300

    implementações de 24/08/2023 da disciplina de LPOO. Ajustado todas as classes da model.

---
+++ src/main/java/br/edu/ifsul/bcc/lpoo/om/model/dao/interfacePersistencia.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.edu.ifsul.bcc.lpoo.om.model.dao;
+
+/**
+ *
+ * @author Saimon AS Rocha
+ */
+public interface interfacePersistencia {
+    
+}
